b5db9b3681a35dd67a6a94d6e128d7429894995d
zhaojc <zhaojc@calis.edu.cn>
2016-09-29 16:00:17 +0800

    test init sql

--- src/com/wsddata/servlet/Install.java
+++ src/com/wsddata/servlet/Install.java
@@ -3,6 +3,10 @@ package com.wsddata.servlet;
 import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -41,7 +45,6 @@ public class Install extends HttpServlet {
 	public void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		String path=getServletContext().getRealPath("/");
-		System.out.println(path);
 		String installFileName=path+"install.html";
 		File file = new File(installFileName);   
 	    // 判断文件是否存在   
@@ -50,10 +53,29 @@ public class Install extends HttpServlet {
 	    } else {
 	        if (file.isFile()) {
 	        	//执行初始化sql
+	        	String driver = "com.mysql.jdbc.Driver";
+	        	String dbhost="localhost"; //用户端获取
+	        	String dbname="courseware";//用户端获取
+	        	String dbuser="courseware";//用户端获取
+	        	String dbpassword="courseware";//用户端获取
+	        	String url = "jdbc:mysql://"+dbhost+":3306/"+dbname;
 	        	
+	        	Connection conn = null;
+	        	try {
+	        	       Class.forName(driver); //classLoader,加载对应驱动
+	        	       conn = (Connection) DriverManager.getConnection(url, dbuser, dbpassword);
+	        	       String sql="insert into photo(title,filepath)values('demo8','images/76977.jpg');";
+	        	       PreparedStatement pstmt = (PreparedStatement)conn.prepareStatement(sql);
+	        	       pstmt.execute();
+	        	       //ResultSet rs = pstmt.executeQuery();
+	        	   } catch (ClassNotFoundException e) {
+	        	       e.printStackTrace();
+	        	   } catch (SQLException e) {
+	        	       e.printStackTrace();
+	        	   }
 	        	
-	            file.delete();
-	            response.sendRedirect("../index.html");
+	            //file.delete();
+	            //response.sendRedirect("../index.html");
 	        }
 	    }
 	}
